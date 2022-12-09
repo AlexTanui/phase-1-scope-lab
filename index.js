ab78d131bd9bd931f38d801dfa2fe7a465c4b54b
Alex <50584983+AlexTanui@users.noreply.github.com>
2022-12-09 12:24:18 +0300

    Update index.js
    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "Dan";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Ian";
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
